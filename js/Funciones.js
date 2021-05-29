7c692b1e6378b2a8bc00731b733c063024f4f1ea
leo blanco <leo_b150@hotmail.com>
2021-05-28 21:43:25 -0300

    Parcial por mitad

---
+++ js/Funciones.js
@@ -0,0 +1,90 @@
+window.addEventListener("load", cargar);
+
+
+function cargar() 
+{
+    SetYears("date-dropdown");
+    console.log();("funciona uwu");
+    var peticionHttp = new XMLHttpRequest();
+    EjecutarGet(peticionHttp);
+    peticionHttp.open("GET", "http://localhost:3000/autos", true);
+    peticionHttp.send();
+}
+
+function EjecutarGet(peticion) 
+{
+    peticion.onreadystatechange = function() 
+    {
+        if (peticion.readyState == 4 && peticion.status == 200) 
+        {
+            
+            var listaAutos = JSON.parse(peticion.responseText);
+            for (var index = 0; index < listaAutos.length; index++) 
+            {
+                console.log(listaAutos[index].make,listaAutos[index].model,listaAutos[index].year);
+                AddElement(listaAutos[index].make, listaAutos[index].model, listaAutos[index].year);
+                
+            }
+        }
+    };
+}
+
+function AddElement(marca, modelo, año) 
+{
+    var cuerpo = GetId("TablaBody");
+    var row = document.createElement("tr");
+    var tdMarca = document.createElement("td");
+    var tdModelo = document.createElement("td");
+    var tdYear = document.createElement("td");
+    var textMarca = document.createTextNode(marca);
+    var textModelo = document.createTextNode(modelo);
+    var textYear = document.createElement("select");
+
+    cuerpo.appendChild(row);
+    row.appendChild(tdMarca);
+    tdMarca.appendChild(textMarca);
+    row.appendChild(tdModelo);
+    tdModelo.appendChild(textModelo);
+    row.appendChild(tdYear);
+    tdYear.appendChild(textYear);
+    textYear.id = Math.random().toString();
+    SetYears(textYear.id);
+}
+
+function SetInputs() 
+{
+    
+}
+function CreateYears() 
+{
+    var currentYear = 2020;    
+    var earliestYear = 2000;     
+    while (currentYear >= earliestYear) {      
+        var dateOption = document.createElement('option');          
+        dateOption.text = currentYear;      
+        dateOption.value = currentYear;        
+        dateDropdown.add(dateOption);      
+        currentYear -= 1;    
+    }
+}
+function SetYears(id) 
+{
+    var dateDropdown  = GetId(id);
+    var currentYear = 2020;    
+    var earliestYear = 2000;     
+    while (currentYear >= earliestYear) {      
+        var dateOption = document.createElement('option');          
+        dateOption.text = currentYear;      
+        dateOption.value = currentYear;        
+        dateDropdown.add(dateOption);      
+        currentYear -= 1;    
+    }
+}
+
+function GetId(object) 
+{
+    if(document.getElementById(object) != null)
+    {
+        return document.getElementById(object);
+    }
+}
